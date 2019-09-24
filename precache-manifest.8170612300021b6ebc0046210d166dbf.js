f3ce96c0547df533e9e8f141ade22403a1b2920b
Denys Kovalishyn <denys.kovalishyn@nordstrom.com>
2019-09-24 17:00:23 +0300

    initial commit

---
+++ precache-manifest.8170612300021b6ebc0046210d166dbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4a06887a4a5ebe26b812ec6721e409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ab5212d8d096571a1d",
+    "url": "/static/css/main.47a2dc2e.chunk.css"
+  },
+  {
+    "revision": "701733e142f041abc323",
+    "url": "/static/js/2.44884f79.chunk.js"
+  },
+  {
+    "revision": "23ab5212d8d096571a1d",
+    "url": "/static/js/main.5e9f7674.chunk.js"
+  },
+  {
+    "revision": "24fda85336f4fdecdb6f",
+    "url": "/static/js/runtime~main.500019a6.js"
+  }
+]);
